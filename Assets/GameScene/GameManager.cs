bf299124d51ee2bd4406aba5fb8cc5c9ab7368ac
Roman Tolok <labuveiksmi91@gmail.com>
2020-01-04 18:28:24 +0200

    pool manager wip

---
+++ Assets/GameScene/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private PoolManager poolManager;
+
+    private void Start()
+    {
+    }
+
+    private void Initialize()
+    {
+    }
+}
