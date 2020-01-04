41c837439fae7cc9fc548ab8ad0547889709f8e1
Roman Tolok <labuveiksmi91@gmail.com>
2020-01-04 20:19:43 +0200

    camera floating on start

--- Assets/GameScene/Scripts/GameManager.cs
+++ Assets/GameScene/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private PoolManager poolManager;
     [SerializeField] private int roadsAtStart = 5;
+    [SerializeField] private float cameraFloatStep = 0.2f;
 
     //Should change this to calculated value, if we using roads with different sizes
     [SerializeField] private Vector3 roadShift;
@@ -33,6 +34,19 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         Initialize();
+        StartCoroutine(PrepareGame());
+    }
+
+    private IEnumerator PrepareGame()
+    {
+        float steps = (Camera.main.transform.position.y - 1) / cameraFloatStep;
+        for (int i = 0; i < steps; i++)
+        {
+            yield return new WaitForEndOfFrame();
+            float cameraShift = Mathf.Lerp(Camera.main.transform.position.y, 1, cameraFloatStep);
+            Debug.Log(cameraShift);
+            Camera.main.transform.position = new Vector3(0, cameraShift, -10);
+        }
     }
 
     private void Initialize()
